18d8e7badb089b7ee55340a404900ede6f39d062
jordan wade <jordanwade2009@live.com>
2016-10-21 16:07:25 -0500

    set up GET routes and changed /users to /profiles

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var axios = require('axios');
 mongoose.connect(process.env.DB_CONN);
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var profile = require('./routes/profile');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/profile', profile);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
