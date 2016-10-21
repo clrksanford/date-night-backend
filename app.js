19823bec0a18c6986696e1c97b53ced3046b6118
Clark Sanford <clrksanford@gmail.com>
2016-10-21 10:56:27 -0500

    Require cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 mongoose.connect(process.env.DB_CONN);
 
