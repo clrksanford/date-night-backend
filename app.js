77942f11b1fb85d981e0274b59d5212b4961595b
jordan wade <jordanwade2009@live.com>
2016-10-21 16:01:33 -0500

    added nightSchema

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
+var axios = require('axios');
 
 mongoose.connect(process.env.DB_CONN);
 
